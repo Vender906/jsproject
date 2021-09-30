7576d35f9a972f9be2c5e025ec28669e6799045c
Vender <romahruslan@gmail.com>
2021-09-30 21:00:23 +0300

    js commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use sсtrict";
+
+// const result = confirm("Are you dick?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "");
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', "");
+// answers[1] = prompt('Как ваше Фамилия?', "");
+// answers[2] = prompt('Сколько вам лет?', "");
+
+// document.write(answers);
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ruslan";
+// alert(`Привет, ${user}`);
+
+// console.log(4 + +"5");
+
+console.log(5 % 2);
+
+console.log(2 * 4 == "8");
+
+const isChecked = true,
+    isClose = false;
+console.log(isChecked  || isClose);
