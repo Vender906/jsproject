a8b6f963f7fe8fa8116d1cffdfc302b17df8b003
Vender <romahruslan@gmail.com>
2021-10-02 12:33:12 +0300

    create add js

--- js/script.js
+++ js/script.js
@@ -22,10 +22,31 @@
 
 // console.log(4 + +"5");
 
-console.log(5 % 2);
+// console.log(5 % 2);
 
-console.log(2 * 4 == "8");
+// console.log(2 * 4 == "8");
 
-const isChecked = true,
-    isClose = false;
-console.log(isChecked  || isClose);
+// const isChecked = true,
+//     isClose = false;
+// console.log(isChecked  || isClose);
+
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', "");
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
